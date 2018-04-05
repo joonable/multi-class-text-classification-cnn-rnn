ff39d844cd5eaf3d0f4156046bcc00cd25ab9179
Joon <joonable2@gmail.com>
2018-04-05 09:25:57 +0900

    first commit

--- data_helper.py
+++ data_helper.py
@@ -14,6 +14,7 @@ from tensorflow.contrib import learn
 
 logging.getLogger().setLevel(logging.INFO)
 
+
 def clean_str(s):
 	s = re.sub(r"[^A-Za-z0-9:(),!?\'\`]", " ", s)
 	s = re.sub(r" : ", ":", s)
@@ -31,17 +32,19 @@ def clean_str(s):
 	s = re.sub(r"\s{2,}", " ", s)
 	return s.strip().lower()
 
+
 def load_embeddings(vocabulary):
 	word_embeddings = {}
 	for word in vocabulary:
 		word_embeddings[word] = np.random.uniform(-0.25, 0.25, 300)
 	return word_embeddings
 
-def pad_sentences(sentences, padding_word="<PAD/>", forced_sequence_length=None):
+
+def pad_sentences(sentences, padding_word = "<PAD/>", forced_sequence_length = None):
 	"""Pad setences during training or prediction"""
-	if forced_sequence_length is None: # Train
+	if forced_sequence_length is None:  # Train
 		sequence_length = max(len(x) for x in sentences)
-	else: # Prediction
+	else:  # Prediction
 		logging.critical('This is prediction, reading the trained sequence length')
 		sequence_length = forced_sequence_length
 	logging.critical('The maximum length is {}'.format(sequence_length))
@@ -51,7 +54,7 @@ def pad_sentences(sentences, padding_word="<PAD/>", forced_sequence_length=None)
 		sentence = sentences[i]
 		num_padding = sequence_length - len(sentence)
 
-		if num_padding < 0: # Prediction: cut off the sentence if it is longer than the sequence length
+		if num_padding < 0:  # Prediction: cut off the sentence if it is longer than the sequence length
 			logging.info('This sentence has to be cut off because it is longer than trained sequence length')
 			padded_sentence = sentence[0:sequence_length]
 		else:
@@ -59,13 +62,17 @@ def pad_sentences(sentences, padding_word="<PAD/>", forced_sequence_length=None)
 		padded_sentences.append(padded_sentence)
 	return padded_sentences
 
+
 def build_vocab(sentences):
 	word_counts = Counter(itertools.chain(*sentences))
-	vocabulary_inv = [word[0] for word in word_counts.most_common()]
-	vocabulary = {word: index for index, word in enumerate(vocabulary_inv)}
+	vocabulary_inv = [word[0] for word in word_counts.most_common()]        #list
+	# {word1 : cnt1, word2 : cnt2 ... } => [word1, word2, ...]
+	vocabulary = {word:index for index, word in enumerate(vocabulary_inv)}      #dict
+	# [word1, word2, ...] => {word1 : 0, word2 : 1... }
 	return vocabulary, vocabulary_inv
 
-def batch_iter(data, batch_size, num_epochs, shuffle=True):
+
+def batch_iter(data, batch_size, num_epochs, shuffle = True):
 	data = np.array(data)
 	data_size = len(data)
 	num_batches_per_epoch = int(data_size / batch_size) + 1
@@ -82,12 +89,14 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
 			end_index = min((batch_num + 1) * batch_size, data_size)
 			yield shuffled_data[start_index:end_index]
 
+
 def load_data(filename):
-	df = pd.read_csv(filename, compression='zip')
-	selected = ['Category', 'Descript']
+	df = pd.read_csv(filename)
+	selected = ['Category', 'Question']
+	# selected = ['Category', 'Descript']
 	non_selected = list(set(df.columns) - set(selected))
 
-	df = df.drop(non_selected, axis=1)
+	df = df.drop(non_selected, axis = 1)
 	df = df.dropna(axis=0, how='any', subset=selected)
 	df = df.reindex(np.random.permutation(df.index))
 
@@ -97,16 +106,24 @@ def load_data(filename):
 	np.fill_diagonal(one_hot, 1)
 	label_dict = dict(zip(labels, one_hot))
 
-	x_raw= df[selected[1]].apply(lambda x: clean_str(x).split(' ')).tolist()
-	y_raw = df[selected[0]].apply(lambda y: label_dict[y]).tolist()
+	x_raw = df[selected[1]].apply(lambda x:x.split(' ')).tolist()
+	# x_raw = df[selected[1]].apply(lambda x:clean_str(x).split(' ')).tolist()
+	y_raw = df[selected[0]].apply(lambda y:label_dict[y]).tolist()
 
 	x_raw = pad_sentences(x_raw)
 	vocabulary, vocabulary_inv = build_vocab(x_raw)
 
 	x = np.array([[vocabulary[word] for word in sentence] for sentence in x_raw])
+	# [
+	#   [1, 3, 3, 2, 5],
+	#   [1, 3, 3, 2, 5],
+	#       ...
+	# 		            ]
 	y = np.array(y_raw)
 	return x, y, vocabulary, vocabulary_inv, df, labels
 
+
 if __name__ == "__main__":
+	# train_file = './data/train.csv.zip'
 	train_file = './data/train.csv.zip'
 	load_data(train_file)
